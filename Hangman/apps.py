87d7d59d391cbdd7b4bdcd02b5839424f224dfd0
S-eng-cmd <shevarvara27@gmail.com>
2025-05-28 21:57:05 +0200

    Update apps.py

--- Hangman/apps.py
+++ Hangman/apps.py
@@ -1,7 +1,7 @@
 from django.apps import AppConfig
 
 
-class BullsAndCowsConfig(AppConfig):
+class Hangman(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "Hangman"
     verbose_name = "Шибениця"
